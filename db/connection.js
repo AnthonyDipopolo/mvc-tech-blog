da6966502abc573c6736400e7fa817981b198186
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-07-28 16:45:09 -0400

    fixed db connection

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,15 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Load environment variables from .env
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize("user_twitter_example", "root", "", {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
     },
-  },
-});
+  });
+
+module.exports = sequelize;
